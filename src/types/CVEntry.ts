0bbe6254c59f19e52341c21d157dd9933698edfc
justDabuK <kevinjust87@gmail.com>
2025-08-04 20:45:06 +0200

    feat: german translations

--- src/types/CVEntry.ts
+++ src/types/CVEntry.ts
@@ -1,8 +1,10 @@
 export type CVEntry = {
   company: string;
   positions: string[];
+  positionen: string[];
   startDate: number;
   endDate: number | string;
   description: string;
+  beschreibung: string;
   skills: string[];
 };
