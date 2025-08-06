6296b1561a17e1888f206064faf5ea26ef8aa594
justDabuK <kevinjust87@gmail.com>
2025-08-06 20:18:29 +0200

    feat: split cv into cv and lebenslauf

--- src/types/CVEntry.ts
+++ src/types/CVEntry.ts
@@ -1,10 +1,8 @@
 export type CVEntry = {
   company: string;
   positions: string[];
-  positionen: string[];
   startDate: number;
   endDate: number | string;
   description: string;
-  beschreibung: string;
   skills: string[];
 };
