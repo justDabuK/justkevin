b8f76af8a99e9a0a5be114aab46e4e8ed8ff1c4c
kevin.just <kevin.just@agile-robots.com>
2025-09-18 15:23:47 +0200

    feat: added new blog and start supporting mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import icon from "astro-icon";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [icon(), mdx()],
   site: "https://justkevin.dev",
   markdown: {
     shikiConfig: {
